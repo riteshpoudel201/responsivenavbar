14a9a67595e82e8f5358abbe8670df11fb9247de
riteshpoudel201 <39463468+riteshpoudel201@users.noreply.github.com>
2022-06-19 20:37:21 +0545

    fixed inner buttons that was not working previously

--- slider.js
+++ slider.js
@@ -13,7 +13,7 @@ const slide = document.querySelector(".carousel-slide");
 const image = document.querySelectorAll("img");
 
 //getting last and first child from selected images
-const lastImage = image[image.length-1];
+const lastImage = image[image.length - 1];
 const firstImage = image[0];
 firstImage.draggable = false;
 console.log(firstImage);
@@ -32,56 +32,58 @@ console.log(size);
 //
 //event handlers
 //
-nextBtn.addEventListener("click",function(event) {nextImage(event)});
-prevBtn.addEventListener("click",function(event){ prevImage(event)});
+nextBtn.addEventListener("click", function (event) {
+    nextImage(event)
+});
+prevBtn.addEventListener("click", function (event) {
+    prevImage(event)
+});
 //innerBtn.addEventListener("click",function(){ viewImage() });
-innerBtn.forEach(inner => {
-    var btn = 0;
-    inner.addEventListener("click", function(){ viewImage(btn) })
-    btn++
+innerBtn.forEach(current => {
+    
+    current.addEventListener("click", function () {
+        viewImage(current)
+    })
 });
 
 //
 //functions
 //
- function nextImage(e) {
+function nextImage(e) {
     slide.style.transition = "transform 0.4s ease-in-out";
-    if(image[count] === lastImage){
-    count;
-    console.log("you are at last position");
-    }
-    else
-    {
+    if (image[count] === lastImage) {
+        count;
+        console.log("you are at last position");
+    } else {
         count++;
-        slide.style.transform = "translate("+ (-size * count) + "px)";
- }
+        slide.style.transform = "translate(" + (-size * count) + "px)";
     }
-    // else{
-    //     count;
-    // }
-    
-    // image[0].style.display = "none";
+}
 
- function prevImage(e) {
+
+function prevImage(e) {
     slide.style.transition = "transform 0.4s ease-in-out";
-    if(image[count] === firstImage){
-    count;
-    console.log("you are at first position");
-    }
-    else{
-    count--;
-    slide.style.transform = "translate("+ (-size * count) + "px)";
-    }
-}
-
-function viewImage(btn){
-    // console.log(innerBtn[btn]);
-    if(innerBtn[btn].dataset.title === image[btn].id){
-        slide.style.transform = "translate("+ (-size * btn) + "px)";
-    }
-    else{
-        console.log("not matched");
+    if (image[count] === firstImage) {
+        count;
+        console.log("you are at first position");
+    } else {
+        count--;
+        slide.style.transform = "translate(" + (-size * count) + "px)";
     }
 }
 
+function viewImage(current) {
+    console.log(current);
+    innerBtn.forEach(buttons =>{
+        if(buttons.dataset.title === current.dataset.title){
+            current.classList.add("active");
+        }
+        else{
+            buttons.classList.remove("active");
+        }
+    })
+    
 
+    slide.style.transition = "transform 0.4s ease-in-out";
+    slide.style.transform = "translate(" + (-size * current.dataset.title) + "px)";
+}
